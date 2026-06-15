94c39f605fec76054c44f8d388a842dfa5d9097e
cs <cs1597616514@gmail.com>
2026-06-15 14:12:49 +0800

    C700 D004 V1.0 10s8.7Ah 20260615

--- tc_ble_single_sdk-V3.4.2.8_Patch_0001/tc_ble_single_sdk/vendor/ble_sample/conf.h
+++ tc_ble_single_sdk-V3.4.2.8_Patch_0001/tc_ble_single_sdk/vendor/ble_sample/conf.h
@@ -42,7 +42,7 @@
 #define C11_AND_C11pro         13
 #define test_default         14
 
-#define FD_BMS_TYPE   D11
+#define FD_BMS_TYPE   C700
 
 #define  BMS_SOFTWARE_VERDION_DEFAULT  	"V1.0" 
 #define  BMS_SERIAL_NUMBER_DEFAULT  	"20260615"
@@ -83,6 +83,7 @@
 #define AFE_ODC2       		(500) 
 #define  BMS_HARDWARE_VERDION_DEFAULT   "C700"
 // #define  BMS_SOFTWARE_VERDION_DEFAULT  	"D004" 
+#define  BMS_SERIAL_NUMBER_DEFAULT  	"D004-20260615"
 #elif (FD_BMS_TYPE == M1PRO)
 #define SeriesNum  (13)
 #define CapacityFactory (116)
