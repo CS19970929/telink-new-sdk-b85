e1a92c70e5dc18f2d82f6e27179a3d1e1f202f7a
CS19970929 <32015803+CS19970929@users.noreply.github.com>
2026-09-12 18:57:04 +0800

    refactor(sif): own timer and expose only scheduler API

--- tc_ble_single_sdk-V3.4.2.8_Patch_0001/tc_ble_single_sdk/vendor/ble_sample/sif_send.h
+++ tc_ble_single_sdk-V3.4.2.8_Patch_0001/tc_ble_single_sdk/vendor/ble_sample/sif_send.h
@@ -1,16 +1,8 @@
 #ifndef SIF_SEND_H_
 #define SIF_SEND_H_
 
-typedef enum
-{
-    SIF_IDLE = 0,
-    SYNC_SIGNAL,
-    SEND_PUBLIC,
-    SEND_DATA,
-    SEND_DATA_COMPLETE,
-    STOP_SIGNAL,
-} SIF_STATE_E;
-
+void sif_timer_init(void);
+void sif_timer_irq_proc(void);
 void sif_send_data_handle(void);
 
 #endif /* SIF_SEND_H_ */
