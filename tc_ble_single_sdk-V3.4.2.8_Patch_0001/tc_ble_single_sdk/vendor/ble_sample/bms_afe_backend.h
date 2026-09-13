8bdd73d6700742876677630e7ba9e51cb4a80785
CS19970929 <32015803+CS19970929@users.noreply.github.com>
2026-09-13 08:24:28 +0800

    feat: select SH3673510 backend for D011

---
+++ tc_ble_single_sdk-V3.4.2.8_Patch_0001/tc_ble_single_sdk/vendor/ble_sample/bms_afe_backend.h
@@ -0,0 +1,12 @@
+#ifndef BMS_AFE_BACKEND_H_
+#define BMS_AFE_BACKEND_H_
+
+#define BMS_AFE_BACKEND_DVC1124     1
+#define BMS_AFE_BACKEND_SH3673510   2
+
+/* HS-D011 production profile: TLSR8251 + SH3673510. */
+#ifndef BMS_AFE_BACKEND
+#define BMS_AFE_BACKEND BMS_AFE_BACKEND_SH3673510
+#endif
+
+#endif /* BMS_AFE_BACKEND_H_ */
