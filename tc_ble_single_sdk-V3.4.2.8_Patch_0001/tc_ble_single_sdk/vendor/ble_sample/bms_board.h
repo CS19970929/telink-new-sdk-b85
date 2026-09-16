3a1474a294845f229f197a5ff5a73673bcdc7b2e
CS19970929 <32015803+CS19970929@users.noreply.github.com>
2026-09-16 15:22:10 +0800

    refactor(d008): expose heater fuse policy from board layer

--- tc_ble_single_sdk-V3.4.2.8_Patch_0001/tc_ble_single_sdk/vendor/ble_sample/bms_board.h
+++ tc_ble_single_sdk-V3.4.2.8_Patch_0001/tc_ble_single_sdk/vendor/ble_sample/bms_board.h
@@ -16,6 +16,8 @@ uint8_t bms_board_charge_source_present(void);
 uint8_t bms_board_heater_supported(void);
 void bms_board_heater_set(uint8_t enabled);
 uint8_t bms_board_heater_fuse_supported(void);
+uint16_t bms_board_heater_off_fault_temp_x10(void);
+uint16_t bms_board_heater_off_fault_confirm_ms(void);
 void bms_board_heater_fuse_fire(void);
 
 #endif /* BMS_BOARD_H_ */
