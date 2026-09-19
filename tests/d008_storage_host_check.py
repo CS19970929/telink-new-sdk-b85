1988e94c4aa798133b028fdb08b2ec41835d2514
CS19970929 <32015803+CS19970929@users.noreply.github.com>
2026-09-19 16:18:47 +0800

    test(d008): include balance macros in storage harness

--- tests/d008_storage_host_check.py
+++ tests/d008_storage_host_check.py
@@ -13,6 +13,8 @@ def main():
     param=source('param.h'); a=param.index('struct PRT_E2ROM_PARAS {'); b=param.index('};',a)+2
     conf=(MOD/'conf.h').read_text()
     macros='\n'.join(re.findall(r'^#define (?:FW_UPGRADE_RESET_\w+|BMS_(?:STATE_SAVE|EVENT_SAVE|STORAGE_RETRY)_INTERVAL_32K)[^\n]*',conf,re.M))
+    feature_header=(MOD/'bms_features.h').read_text()
+    macros+='\n'+'\n'.join(re.findall(r'^#define BMS_BALANCE_(?:ENABLE_DEFAULT|START_VOLTAGE_MV_DEFAULT|START_DELTA_MV_DEFAULT|STOP_DELTA_MV_DEFAULT|CELL_PLAUSIBLE_MIN_MV|SUSPECT_DELTA_MV)[^\n]*',feature_header,re.M))
     headers='\n'.join(source(n) for n in ['bms_soc_defs.h','bms_state_store.h','soc_kv_store.h','SocEnhance.h','bms_afe_hw_profile.h','bms_config_store.h','bms_cold_kv_store.h','bms_event_log.h','bms_storage_platform.h'])
     units='\n'.join(source(n) for n in ['bms_config_store.c','bms_state_store.c','bms_event_log.c','param.c'])
     fixture=(ROOT/'tests/fixtures/d008_storage/stores.c').read_text()
