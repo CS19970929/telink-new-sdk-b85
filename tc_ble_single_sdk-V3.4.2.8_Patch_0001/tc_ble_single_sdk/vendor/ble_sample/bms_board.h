3a818aa31b44aa72d3f81c7e536d3809ca27e68d
CS19970929 <32015803+CS19970929@users.noreply.github.com>
2026-09-19 16:53:23 +0800

    feat(d013): expose feature capability gates

--- tc_ble_single_sdk-V3.4.2.8_Patch_0001/tc_ble_single_sdk/vendor/ble_sample/bms_board.h
+++ tc_ble_single_sdk-V3.4.2.8_Patch_0001/tc_ble_single_sdk/vendor/ble_sample/bms_board.h
@@ -13,6 +13,8 @@
 void bms_board_features_init(void);
 uint8_t bms_board_charge_source_present(void);
 uint8_t bms_board_heater_supported(void);
+uint8_t bms_board_heater_allowed(void);
+uint8_t bms_board_balance_supported(void);
 void bms_board_heater_set(uint8_t enabled);
 
 #endif /* BMS_BOARD_H_ */
